09dc0ee3ce1418b678f93b1564a5dafce2a95b92
airyland <airyland@qq.com>
2014-11-05 16:40:55 +0800

    re code

--- examples/index.md
+++ examples/index.md
@@ -5,26 +5,36 @@
 ## Normal usage
 
 ````html
- <script type="seedit/template" id="ooxx-tpl-2">
+ <script type="seedit/template" id="ooxx-tpl-70">
  <ul>
-    {{#each ad_show_info}}
-    <li class="ooxx-item-wrap" style="display:block;background-color:{{ad_expand.backgroundColor}}">
-        <div class="ooxx-item" style="display:block;background:url({{ad_content}}) top center no-repeat">
-            <a href="{{ad_link}}" target="_blank"
-                       data-track="OOXX/顶部通栏/{{ad_name}}" style="display:block;width:100%;height:250px;">&nbsp;</a>
+    {{#each list}}
+    <li class="ooxx-item-wrap" style="display:block;background-color:{{expand.backgroundColor}}">
+        <div class="ooxx-item" style="display:block;background:url({{content}}) top center no-repeat">
+            <a href="{{link}}" target="_blank"
+                       data-track="OOXX/顶部通栏/{{name}}" style="display:block;width:100%;height:250px;">&nbsp;</a>
         </div>
     </li>
     {{/each}}
 </ul>
 </script>
 
-<div data-sid="2" id="ooxx-2"></div>
+<div data-sid="70" id="ooxx-70" data-page="" data-block="" data-effect="slide" class="ooxx-loading"></div>
 
 
 ````
 
 ````javascript
 seajs.use('index', function(seeditOoxx) {
-    seeditOoxx.render('ooxx-2');
+    var ooxx = new seeditOoxx('70')
+    ooxx.
+    on('space_fetched',function(data){
+    	// 单个广告位数据
+    	// 多次触发
+    }).
+    on('space_fetched_time',function(time){
+    	// 获取到了时间，用于性能监控
+    	// 触发一次
+    	console.log(time);
+    });
 });
 ````
--- index.js
+++ index.js
@@ -1,34 +1,72 @@
 var jsonp = require('jsonp'),
     Config = require('seedit-config'),
     Handlebars = require('handlebarser'),
-    baseAPI = Config.getSiteUrl('ad') + '/restful/cast',
+    baseAPI = 'http://common.bozhong.com/restful/ad',
     sidAPI = baseAPI + '/space.jsonp',
     gidAPI = baseAPI + '/group.jsonp?gid=';
 
-// get by sid
+var eventor = require('eventor');
+
+// @todo 效果选择
+// @todo 多图时切换成轮播效果
+
+// fetching data
 var getBySid = exports.getBySid = function(sid, callback) {
-    console.log(sidAPI + sid);
+    var begin = new Date().getTime();
     jsonp(sidAPI, {
-        space_id: sid
+        space: sid
     }, '__c', function(data) {
-        callback && callback(data);
+        var length = new Date().getTime() - begin;
+        callback && callback(data.data, length);
     });
 };
 
-// get by gid
-var getByGid = exports.getByGid = function(gid) {
 
-};
-
-// auto run
-var init = exports.render = function(target) {
+// render data
+var render = exports.render = function(target, data) {
     var $target = document.getElementById(target);
     var sid = $target.getAttribute('data-sid');
     var tpl = document.getElementById('ooxx-tpl-' + sid).innerHTML;
-    getBySid(sid, function(data) {
-        console.log(data);
-        var html = Handlebars.compile(tpl)(data.data[0]);
-        console.log(html);
-        $target.innerHTML = html;
+    tpl = filterParams(tpl);
+    var html = Handlebars.compile(tpl)(data);
+    $target.innerHTML = html;
+};
+
+// 变换参数名，在模板中隐藏 ad_的前缀
+function filterParams(tpl) {
+    var arrays = ['id', 'adm_id', 'name', 'type', 'content', 'link', 'expand'];
+    for (var i = 0; i < arrays.length; i++) {
+        var item = arrays[i];
+        tpl = tpl.replace(new RegExp('{{' + item, 'g'), '{{ad_' + item);
+    }
+    return tpl;
+};
+
+function ooxx(sids) {
+    var _this = this;
+    _this.sids = sids;
+    _this.fetch(sids);
+    _this.on('space_fetched', function(data) {
+        render('ooxx-' + data.id, {
+            list: data.data
+        });
     });
 };
+
+eventor.mixTo(ooxx);
+
+ooxx.prototype.fetch = function(sids) {
+    var _this = this;
+    getBySid(sids, function(data, time) {
+        _this.emit('space_fetched_time', time);
+        for (var i in data) {
+            var item = data[i][0];
+            _this.emit('space_fetched', {
+                id: i.replace('space_', ''),
+                data: item.space_ad_info
+            });
+        }
+    });
+};
+
+module.exports = ooxx;
