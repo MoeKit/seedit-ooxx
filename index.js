e46f185574c96a79435bcc8f111deac921ad0560
airyland <airyland@qq.com>
2015-01-15 20:44:20 +0800

    reformat

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var jsonp = require('jsonp'),
     sidAPI = baseAPI + '/space.jsonp',
     gidAPI = baseAPI + '/group.jsonp?gid=';
 
-var Uuid = require('i-tracker/lib/uuid');
+var $ = require('jquery');
+
+var Uuid = require('i-tracker/lib/iuuid');
 Uuid.init({
     cookieDomain: Config.getMainDomain()
 });
@@ -15,25 +17,15 @@ var eventor = require('eventor');
 
 // @todo 效果选择
 // @todo 多图时切换成轮播效果
-
-// fetching data
-var getBySid = exports.getBySid = function(sid, callback) {
-    var begin = new Date().getTime();
-    jsonp(sidAPI, {
-        space: sid,
-        cookie: uuid
-    }, '__c', function(data) {
-        var length = new Date().getTime() - begin;
-        callback && callback(data.data, length);
-    });
-};
-
-
 // render data
 var render = exports.render = function(target, data, cb) {
     var $target = document.getElementById(target);
     var sid = $target.getAttribute('data-sid');
     var tpl = defaultTpl;
+    // attach sid 
+    for (var i = 0; i < data.list.length; i++) {
+        data.list[i].sid = sid;
+    }
     // check if tpl exists
     var tplTarget = document.getElementById('ooxx-tpl-' + sid);
     if (tplTarget) {
@@ -45,20 +37,19 @@ var render = exports.render = function(target, data, cb) {
     cb && cb(data, tpl);
 };
 
-// 变换参数名，在模板中隐藏 ad_的前缀
-function filterParams(tpl) {
-    var arrays = ['id', 'adm_id', 'name', 'type', 'content', 'link', 'expand'];
-    for (var i = 0; i < arrays.length; i++) {
-        var item = arrays[i];
-        tpl = tpl.replace(new RegExp('{{' + item, 'g'), '{{ad_' + item);
-    }
-    return tpl;
-};
+
 
 function ooxx(sids) {
     var _this = this;
     _this.sids = sids;
     _this.fetch(sids);
+    _this._on_fetched();
+};
+
+eventor.mixTo(ooxx);
+
+ooxx.prototype._on_fetched = function(){
+	var _this = this;
     _this.
     on('space_fetched', function(data) {
         render('ooxx-' + data.id, {
@@ -71,7 +62,7 @@ function ooxx(sids) {
             var target = null;
             var slide = 'div';
             var dots = document.getElementById('ooxx-' + data.id).getAttribute('data-dots') === 'true';
-            if (datas.list.length) {
+            if (datas.list.length>1) {
                 if (/<li>/.test(tpl)) {
                     target = '#ooxx-' + data.id + '-ul';
                     slide = 'li';
@@ -82,11 +73,14 @@ function ooxx(sids) {
                     target = '#ooxx-' + data.id;
                     slide = 'a';
                 }
+
+
                 seajs.use('slicker/1.3.14/index', function(Slicker) {
                     new Slicker(target, {
                         slide: slide,
                         autoplay: true,
                         dots: dots,
+                        dotsClass: 'slick-dots-wrap',
                         fade: false
                     });
                 });
@@ -97,9 +91,8 @@ function ooxx(sids) {
     on('space_rendered', function(id) {
         document.getElementById('ooxx-' + id).setAttribute('data-rendered', 'true');
     });
+    return _this;
 };
-
-eventor.mixTo(ooxx);
 
 ooxx.prototype.fetch = function(sids) {
     var _this = this;
@@ -125,15 +118,45 @@ ooxx.prototype.fetch = function(sids) {
     });
 };
 
+
+// 获取所有的sid, 目前只处理空标签，不为空说明为cms输出
+// @todo 获取广告后对比替换cms的输出
 function getSidByDom(callback) {
     var sids = [];
-    seajs.use('jquery', function($) {
-        $('div[data-sid]').each(function() {
-            sids.push($(this).data('sid'));
-        });
-        callback(sids.join('|'));
+    $('div[data-sid]').each(function() {
+    	if(!$(this).text()){
+    		sids.push($(this).data('sid'));
+    	}else{
+    		// 处理成轮播
+    	}
+    });
+    callback(sids.join('|'));
+};
+
+
+// 变换参数名，在模板中隐藏 ad_的前缀
+function filterParams(tpl) {
+    var arrays = ['id', 'adm_id', 'name', 'type', 'content', 'link', 'expand'];
+    for (var i = 0; i < arrays.length; i++) {
+        var item = arrays[i];
+        tpl = tpl.replace(new RegExp('{{' + item, 'g'), '{{ad_' + item);
+    }
+    return tpl;
+};
+
+
+// 根据sid获取广告数据
+var getBySid = exports.getBySid = function(sid, callback) {
+    var begin = new Date().getTime();
+    jsonp(sidAPI, {
+        space: sid,
+        cookie: uuid
+    }, '__c', function(data) {
+        var length = new Date().getTime() - begin;
+        callback && callback(data.data, length);
     });
 };
+
 
 module.exports = ooxx;
 module.exports.render = function() {
